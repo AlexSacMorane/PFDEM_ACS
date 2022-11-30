5ae8f1bb751ea589c7ea22bf9da05666d49fa07f
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2022-11-30 11:14:38 +0100

    Correct User

--- User.py
+++ User.py
@@ -53,9 +53,9 @@ def All_parameters():
     'x_L' : x_L,
     'y_L' : y_L,
     'grain_discretisation' : grain_discretisation,
-    'Emec_M' : np.array(np.zeros((len(dict_sample['y_L']),len(dict_sample['x_L'])))),
-    'Eche_M' : np.array(np.zeros((len(dict_sample['y_L']),len(dict_sample['x_L'])))),
-    'Ed_M' : np.array(np.zeros((len(dict_sample['y_L']),len(dict_sample['x_L']))))
+    'Emec_M' : np.array(np.zeros((y_L,x_L))),
+    'Eche_M' : np.array(np.zeros((y_L,x_L))),
+    'Ed_M' : np.array(np.zeros((y_L,x_L)))
     }
 
     #---------------------------------------------------------------------------
