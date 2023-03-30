b72594764afe0d550cb74fa868d5ccb4016d63fb
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-03-30 14:23:02 +0200

    corrdction

--- PostProcess/postprocess.py
+++ PostProcess/postprocess.py
@@ -363,7 +363,7 @@ for i_overlap in range(len(overlap_L)):
         plt.title('overlap = '+str(overlap_L[i_overlap])+r' $\kappa_c$ = '+str(kappa_c_L[i_kappa_c]))
         for i_chi in range(len(chi_L)):
             i_list = i_overlap*len(chi_L)*len(kappa_c_L) + i_chi*len(kappa_c_L) + i_kappa_c
-            plt.plot(L_L_t[i_list][:-1], L_L_ed_mec[i_list], label=r'$\chi$ = '+str(overlap_L[i_chi]), color = color_L[i_chi])
+            plt.plot(L_L_t[i_list][:-1], L_L_ed_mec[i_list], label=r'$\chi$ = '+str(chi_L[i_chi]), color = color_L[i_chi])
             plt.plot(L_L_t[i_list][:-1], L_L_ed_che[i_list][:-1], color = color_L[i_chi], linestyle = '-.')
         plt.legend()
 plt.savefig('chi_influence/times_ed_mec_che.png')
