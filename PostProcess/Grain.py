609310116c4ed65f3d1819c51d31626fc2fdebc2
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-04-04 10:27:16 +0200

    Correction

--- PostProcess/Grain.py
+++ PostProcess/Grain.py
@@ -113,7 +113,7 @@ class Grain:
 
         for l in range(i_y_min,i_y_max+1):
             for c in range(i_x_min,i_x_max+1):
-                y = dict_sample['y_L'][-1-l]
+                y = dict_sample['y_L'][l]
                 x = dict_sample['x_L'][c]
                 p = np.array([x,y])
                 r = np.linalg.norm(self.center - p)
@@ -125,7 +125,7 @@ class Grain:
                 L_theta_R_i = list(abs(np.array(self.l_theta_r)-theta))
                 R = self.l_r[L_theta_R_i.index(min(L_theta_R_i))]
                 #build etai_M
-                self.etai_M[l][c] = Owntools.Cosine_Profile(R,r,dict_material['w'])
+                self.etai_M[-1-l][c] = Owntools.Cosine_Profile(R,r,dict_material['w'])
 
     #---------------------------------------------------------------------------
 
